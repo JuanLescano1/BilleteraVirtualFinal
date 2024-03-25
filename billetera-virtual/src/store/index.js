4976c64444941feb9207bc8c8a0c20375d148c72
Juan Cruz Lescano <juancruzlescano@hotmail.com>
2024-03-25 15:47:32 -0300

    Actualizando la CreateStore

--- billetera-virtual/src/store/index.js
+++ billetera-virtual/src/store/index.js
@@ -3,16 +3,25 @@ import { createStore } from "vuex";
 export default createStore({
   state: {
     usuarioAutenticado: false,
+    usuarios: {},
   },
-  getters: {},
+  getters: {
+    usuarioPorId: (state) => (id) => {
+      return state.usuarios[id];
+    },
+  },
   mutations: {
     EstablecerAutenticado(state, value) {
       state.usuarioAutenticado = value;
+    },
+    agregarUsuario (state, {id, datos }) {
+      state.usuarios = datos;
     },
   },
   actions: {
     inicio({ commit }) {
       commit("EstablecerAutenticado", true);
+      commit("AgregarUsuario", { id: idUsuario, datos: {} });
     },
     cierre({ commit }) {
       commit("EstablecerAutenticado", false);
