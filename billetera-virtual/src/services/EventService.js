ea0275358174a84539cebcc3c00d8abfce7381d7
Juan Cruz Lescano <juancruzlescano@hotmail.com>
2024-04-08 15:26:55 -0300

    llamadoApiCorreggiones

--- billetera-virtual/src/services/EventService.js
+++ billetera-virtual/src/services/EventService.js
@@ -4,9 +4,34 @@ const api = axios.create({
   baseURL: "https://criptoya.com/api",
 });
 export default {
-    llamadoExchanges(exchange, moneda, unidad) {
-      const url = `/${exchange}/${moneda}/${unidad}/`;
-  
-      return api2.get(url);
-    },
+  async Bitso(moneda) {
+    try {
+      const url = `/bitsoalpha/${moneda}/ars}`;
+      const response = await api.get(url);
+      return response.data;
+    } catch (error) {
+      console.error("Error al llamar a la API:", error);
+      throw error;
+    }
+  },
+  async Cryptomkt(moneda) {
+    try {
+      const url = `/cryptomkt/${moneda}/ars}`;
+      const response = await api.get(url);
+      return response.data;
+    } catch (error) {
+      console.error("Error al llamar a la API:", error);
+      throw error;
+    }
+  },
+  async Argen(moneda) {
+    try {
+      const url = `/argenbtc/${moneda}/ars}`;
+      const response = await api.get(url);
+      return response.data;
+    } catch (error) {
+      console.error("Error al llamar a la API:", error);
+      throw error;
+    }
+  },
 };
--- billetera-virtual/src/views/CryptosView.vue
+++ billetera-virtual/src/views/CryptosView.vue
@@ -1,51 +1,91 @@
 <template>
-  <div></div>
+  <div>
+    <h1>Monedas del {{ exchanges1 }}</h1>
+    <ul>
+      <li v-for="moneda in monedasBitso" :key="moneda">
+        {{ moneda }} - Ask {{ exchangeRates[`Bitso_${moneda}`]?.ask }}
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
+import eventService from "@/services/EventService.js";
+
 export default {
   data() {
     return {
-      exchanges: {},
-      monedas: [
-        "btc",
-        "eth",
-        "usdt",
-        "usdc",
-        "dai",
-        "uxd",
-        "usdp",
-        "wld",
-        "bnb",
-        "sol",
-        "xrp",
+      exchanges1: "bitsoalpha",
+      exchanges2: "cryptomkt",
+      exchanges3: "argenbtc",
+      monedasBitso: [
         "ada",
-        "avax",
+        "bat",
+        "bch",
+        "btc",
+        "dai",
         "doge",
-        "trx",
-        "link",
         "dot",
+        "eth",
+        "link",
+        "ltc",
+        "mana",
         "matic",
         "shib",
-        "ltc",
-        "bch",
-        "eos",
-        "xlm",
-        "ftm",
-        "aave",
-        "uni",
-        "algo",
-        "bat",
-        "paxg",
-        "cake",
-        "axs",
-        "slp",
-        "mana",
-        "sand",
-        "chz",
+        "sol",
+        "trx",
+        "usdc",
+        "usdt",
+        "xrp",
       ],
+      monedasArgen: ["btc", "dai", "eth", "usdt"],
+      monedasCryptomkt: [
+        "aave",
+        "ada",
+        "algo",
+        "avax",
+        "bch",
+        "bnb",
+        "btc",
+        "dai",
+        "dot",
+        "eos",
+        "eth",
+        "link",
+        "ltc",
+        "matic",
+        "paxg",
+        "shib",
+        "sol",
+        "trx",
+        "uni",
+        "usdc",
+        "usdt",
+        "xlm",
+        "xrp",
+      ],
+      exchangeRates: {},
     };
   },
-  methods: {},
+  methods: {
+    llamadoApi() {
+      this.exchangeRates = {};
+      for (let moneda of this.monedasBitso) {
+        eventService.Bitso(moneda).then((response) => {
+          this.exchangeRates[`Bitso_${moneda}`] = response.data;
+        });
+      }
+      for (let moneda of this.monedasArgen) {
+        eventService.Argen(moneda).then((response) => {
+          this.exchangeRates[`Argen_${moneda}`] = response.data;
+        });
+      }
+      for (let moneda of this.monedasCryptomkt) {
+        eventService.Cryptomkt(moneda).then((response) => {
+          this.exchangeRates[`Cryptomkt_${moneda}`] = response.data;
+        });
+      }
+    },
+  },
 };
 </script>
 <style></style>
