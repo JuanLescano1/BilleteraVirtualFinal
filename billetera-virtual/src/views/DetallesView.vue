9303eab49b70688c48a55a87501ff88311a5c70a
Juan Cruz Lescano <juancruzlescano@hotmail.com>
2024-04-27 16:41:23 -0300

    1

--- billetera-virtual/src/views/DetallesView.vue
+++ billetera-virtual/src/views/DetallesView.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="detalle">
     <h1>Detalles de la moneda: {{ $route.params.moneda }}</h1>
-    <h2>Nombre: {{ detalle && detalle.data.ask }}</h2>
+    <h2>Precio: {{ detalle && detalle.data.ask }}</h2>
     <h2>Precio: {{ detalle && detalle.data.bid }}</h2>
   </div>
 </template>
